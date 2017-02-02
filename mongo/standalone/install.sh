36b47d14a1d4a378c310e95139c8ebbec0fe7bca
Jumath <UJUMARO@PEROOT.COM>
2017-02-02 09:54:52 +0530

    mongodb standalone installation script

---
+++ mongo/standalone/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Install mongodb 3.4 in Ubuntu 16.04
+
+dbPath=/data
+mongoUser=mongodb
+
+if [ $EUID != 0 ]
+  then echo "Run the script as root"
+  exit
+fi
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+apt-get update
+apt-get install -y mongodb-org
+
+mkdir $dbPath
+chown $mongoUser:$mongoUser $dbPath
+
+sed -i "s|/var/lib/mongodb|$dbPath|" /etc/mongod.conf
+sed -i "s|bindIp: 127.0.0.1|#bindIp: 127.0.0.1|" /etc/mongod.conf
+
+systemctl start mongod
+systemctl enable mongod
